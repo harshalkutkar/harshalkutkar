f8d69fe4cbb8613155ab8c91eecb7d3e09609c4c
harshalkutkar <harshalkutkar@gmail.com>
2015-03-24 15:51:27 -0700

    Added Xintong PM Doc March 24

---
+++ Platformer.spritebuilder/Pipe.h
@@ -0,0 +1,13 @@
+//
+//  Pipe.h
+//  Platformer
+//
+//  Created by Harsh Alkutkar on 3/24/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Pipe : CCSprite
+
+@end
---
+++ Platformer.spritebuilder/Pipe.m
@@ -0,0 +1,13 @@
+//
+//  Pipe.m
+//  Platformer
+//
+//  Created by Harsh Alkutkar on 3/24/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Pipe.h"
+
+@implementation Pipe
+
+@end
